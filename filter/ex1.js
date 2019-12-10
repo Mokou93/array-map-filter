2310e5d62006bbf65917b0b491e3c84ba28eb3ac
Koullou Mohamed <koullou.mohamed@gmail.com>
2019-12-10 15:08:37 +0100

    exo 1 filter fini

--- filter/ex1.js
+++ filter/ex1.js
@@ -10,10 +10,11 @@ Sortie attendue:
   [7, 2, 0, 3, 11]
 
  */
-
+const numbers = [7, -4, 2, 0, -10, 3, 11];
 function getPositiveNumbers(numbers) {
+  numbers.filter(n => n > 0);
 }
-
+return getPositiveNumbers(numbers);
 // Si besoin, tu peux toujours visualiser le résultat avec console.log,
 // mais alors, il faut alors lancer l'exercice avec node filter/ex1
 // console.log(getPositiveNumbers([-2, -1, 0, 1, 2]));
